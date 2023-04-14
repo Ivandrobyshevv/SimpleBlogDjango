15d381b1ad7d0dd61283332f9be003b91418862f
Ivan Drobyshev <drobyshev_ivan00@mail.ru>
2023-04-15 01:00:48 +0500

    added PublishedManager

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Черновик'
@@ -17,10 +22,13 @@ class Post(models.Model):
     status = models.CharField("Статус", max_length=2, choices=Status.choices, default=Status.DRAFT)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts", verbose_name="Автор")
 
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Meta:
         verbose_name = "Пост"
         verbose_name_plural = "Посты"
         ordering = ["-publish"]
 
     def __str__(self):
-        return self.title[::30]
+        return self.title[:30]
