a7342495789f31f1a9c921ec9abae3dacec9fa6c
Ivan Drobyshev <drobyshev_ivan00@mail.ru>
2023-04-16 21:28:16 +0500

    added Markdown

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.fields import related
 from django.urls import reverse
 from django.utils import timezone
 from taggit.managers import TaggableManager
